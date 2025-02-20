3c5133121516641103f8697f4eb3a8d2fad9cd70
Glen-Rodrigues <glenandrew.7r@gmail.com>
2025-02-21 00:26:48 +0530

    Create Sol1_Output.c

---
+++ Assignments/Sol1_Output.c
@@ -0,0 +1,7 @@
+Enter number of students: 2
+Enter details (Roll No, Name, Course, Marks):
+Student 1: 101 Alice Aeronautical 89
+Student 2: 102 Bob Mechanical 95
+Enter Roll No to search: 102
+Student Found: Bob, Mechanical, Marks: 95
+Record saved in Students.txt
